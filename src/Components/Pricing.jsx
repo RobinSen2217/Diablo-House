15ff7057b6d6370a5d48a83da7be42226ee00db8
Robin Sen <robinsen2217@gmail.com>
2023-08-11 23:11:28 +0530

    removed pricingend and used state

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -1,31 +1,38 @@
+import React,{useState} from 'react'
 
-      import React from 'react'
-import { Link } from 'react-router-dom'
 
-function PricingEnd() {
+function Pricing() {
+  const [pricing,setPricing]=useState(1)
+  const Weekday=()=>{
+    setPricing(1)
+  }
+  const Weekend=()=>{
+    setPricing(2)
+  }
   return (
     <div id='ticket'>
 <section className="text-gray-400  body-font overflow-hidden ">
   <div className="container py-24 min-[1240px]:w-2/3 mx-auto">
     <div className="flex flex-col text-center w-full mb-20 justify-between">
       <h1 className=" text-7xl font-medium title-font mb-2 text-white font-Mystery">TICKETS</h1>
-      <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-slate-300">All tickets for kids under the age of 10 are 50% off.</p>
+      <p className="lg:w-2/3 mx-auto leading-relaxed  text-base text-slate-300">All tickets for kids under the age of 10 are 50% off.</p>
 
 
       <div className="flex mx-auto border-2 border-orange-500 rounded overflow-hidden mt-6">
-        <Link to='/'className="py-1 px-4 text-gray-300 focus:outline-none hover:cursor-pointer">Weekdays</Link>
-        <Link to='/end' className="py-1 px-4 bg-orange-500 text-white focus:outline-none hover:cursor-pointer">Weekends</Link>
+        <button type='btn' onClick={Weekday} className={`py-1 px-4 text-white ${pricing==1?'bg-orange-500  focus:outline-none':'text-gray-300 focus:outline-none'}`}>Weekdays</button>
+        <button type='btn' onClick={Weekend} className={`py-1 px-4 text-white ${pricing==2?'bg-orange-500  focus:outline-none':'text-gray-300 focus:outline-none'}`}>Weekends</button>
       </div>
+
+
     </div>
     <div className="flex flex-col min-[835px]:grid min-[835px]:grid-cols-2 min-[835px]:grid-rows-1  w-full mx-auto">
-    
       <div className="p-4 w-[400px] mx-auto">
         <div className="h-[380px] p-6 rounded-lg border-2 border-orange-500 flex flex-col relative overflow-hiddenbg-black/70 shadow-md shadow-orange-700 bg-black/70">
           <span className="bg-orange-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span>
           <h2 className="text-sm tracking-widest text-gray-400 title-font mb-1 font-medium">STANDARD</h2>
           <h1 className="text-5xl text-white leading-none flex items-center pb-4 mb-4 border-b border-gray-800">
             <span>	
-&#8377;350</span>
+            &#8377;{pricing==1?'500':'350'}</span>
             <span className="text-lg ml-1 font-normal text-gray-400">/head</span>
           </h1>
           <p className="flex items-center text-gray-400 mb-2">
@@ -33,7 +40,7 @@ function PricingEnd() {
               <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
-              </span><span>Entry to <span className='text-yellow-400'>Entrance of Hell</span> & <span className='text-yellow-400'>Slaughter Asylum</span>(only on weekends)</span>
+            </span><span>Entry to <span className='text-yellow-400'>Entrance of Hell</span> & <span className='text-yellow-400'>Slaughter Asylum</span>(only on weekends)</span>
           </p>
           <p className="flex items-center text-gray-400 mb-2">
             <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-orange-500 text-gray-900 rounded-full flex-shrink-0">
@@ -47,17 +54,17 @@ function PricingEnd() {
               <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
-            </span><span>Free <span className='text-orange-600'>Souvenir Hat</span> for the <span className='text-yellow-400'>first 7 days of opening</span></span>
+            </span><span>Free <span className=' text-orange-600'>Souvenir Hat</span> for the <span className='text-yellow-400'>first 7 days of opening</span></span>
           </p>
-
+      
         </div>
       </div>
-       <div className="p-4  w-[400px] mx-auto">
+      <div className="p-4  w-[400px] mx-auto">
         <div className="h-[380px] p-6 rounded-lg border-2 bg-black/70 shadow-md shadow-slate-700 border-gray-700 flex flex-col relative overflow-hidden">
           <h2 className="text-sm tracking-widest text-gray-400 title-font mb-1 font-medium">VIP</h2>
           <h1 className="text-5xl text-white leading-none flex items-center pb-4 mb-4 border-b border-gray-800">
             <span>	
-&#8377;700</span>
+&#8377;{pricing==1?'900':'700'}</span>
             <span className="text-lg ml-1 font-normal text-gray-400">/head</span>
           </h1>
           <p className="flex items-center text-gray-400 mb-2">
@@ -90,7 +97,6 @@ function PricingEnd() {
           </p>
         </div>
       </div>
-
       </div>
   </div>
 </section>      
@@ -98,4 +104,4 @@ function PricingEnd() {
   )
 }
 
-export default PricingEnd
+export default Pricing
